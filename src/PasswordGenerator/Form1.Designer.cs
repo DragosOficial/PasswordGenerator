e54c19b0064092ab2442d3a37d683f2903d3cecf
Dragos <77962711+DragosOficial@users.noreply.github.com>
2022-05-08 18:56:15 +0200

    Add files via upload

---
+++ src/PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.includeNums = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.generateGUID = new System.Windows.Forms.CheckBox();
+            this.charTypeProbEqualButton = new System.Windows.Forms.RadioButton();
+            this.restoreDefault = new System.Windows.Forms.Button();
+            this.specCharacters = new System.Windows.Forms.TextBox();
+            this.passwordLengthBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.includeLowercase = new System.Windows.Forms.CheckBox();
+            this.includeCaptial = new System.Windows.Forms.CheckBox();
+            this.includeSpecCharctor = new System.Windows.Forms.CheckBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.aboutButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.savePasswordButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+
+            this.includeNums.AutoSize = true;
+            this.includeNums.Location = new System.Drawing.Point(1, 29);
+            this.includeNums.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.includeNums.Name = "includeNums";
+            this.includeNums.Size = new System.Drawing.Size(76, 24);
+            this.includeNums.TabIndex = 0;
+            this.includeNums.Text = "Liczby";
+            this.includeNums.UseVisualStyleBackColor = true;
+
+            this.groupBox1.Controls.Add(this.generateGUID);
+            this.groupBox1.Controls.Add(this.charTypeProbEqualButton);
+            this.groupBox1.Controls.Add(this.restoreDefault);
+            this.groupBox1.Controls.Add(this.specCharacters);
+            this.groupBox1.Controls.Add(this.passwordLengthBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.includeLowercase);
+            this.groupBox1.Controls.Add(this.includeCaptial);
+            this.groupBox1.Controls.Add(this.includeSpecCharctor);
+            this.groupBox1.Controls.Add(this.includeNums);
+            this.groupBox1.Location = new System.Drawing.Point(11, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(413, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opcje";
+
+            this.generateGUID.AutoSize = true;
+            this.generateGUID.Location = new System.Drawing.Point(15, 166);
+            this.generateGUID.Name = "generateGUID";
+            this.generateGUID.Size = new System.Drawing.Size(226, 24);
+            this.generateGUID.TabIndex = 7;
+            this.generateGUID.Text = "Generuj identyfikator GUID";
+            this.generateGUID.UseVisualStyleBackColor = true;
+            this.generateGUID.CheckedChanged += new System.EventHandler(this.generateGUID_CheckedChanged);
+
+            this.charTypeProbEqualButton.AutoSize = true;
+            this.charTypeProbEqualButton.Location = new System.Drawing.Point(15, 137);
+            this.charTypeProbEqualButton.Margin = new System.Windows.Forms.Padding(2);
+            this.charTypeProbEqualButton.Name = "charTypeProbEqualButton";
+            this.charTypeProbEqualButton.Size = new System.Drawing.Size(363, 24);
+            this.charTypeProbEqualButton.TabIndex = 6;
+            this.charTypeProbEqualButton.TabStop = true;
+            this.charTypeProbEqualButton.Text = "Każdy znak ma równe szanse na pojawienia się";
+            this.charTypeProbEqualButton.UseVisualStyleBackColor = true;
+
+            this.restoreDefault.Location = new System.Drawing.Point(296, 59);
+            this.restoreDefault.Margin = new System.Windows.Forms.Padding(2);
+            this.restoreDefault.Name = "restoreDefault";
+            this.restoreDefault.Size = new System.Drawing.Size(109, 34);
+            this.restoreDefault.TabIndex = 5;
+            this.restoreDefault.Text = "Zresetuj";
+            this.restoreDefault.UseVisualStyleBackColor = true;
+            this.restoreDefault.Click += new System.EventHandler(this.restoreDefault_Click);
+
+            this.specCharacters.Location = new System.Drawing.Point(139, 63);
+            this.specCharacters.Margin = new System.Windows.Forms.Padding(2);
+            this.specCharacters.Name = "specCharacters";
+            this.specCharacters.Size = new System.Drawing.Size(148, 27);
+            this.specCharacters.TabIndex = 4;
+
+            this.passwordLengthBox.Location = new System.Drawing.Point(117, 101);
+            this.passwordLengthBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordLengthBox.MaxLength = 3;
+            this.passwordLengthBox.Name = "passwordLengthBox";
+            this.passwordLengthBox.Size = new System.Drawing.Size(91, 27);
+            this.passwordLengthBox.TabIndex = 3;
+
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 104);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Długośc hasła：";
+
+            this.includeLowercase.AutoSize = true;
+            this.includeLowercase.Location = new System.Drawing.Point(250, 29);
+            this.includeLowercase.Margin = new System.Windows.Forms.Padding(2);
+            this.includeLowercase.Name = "includeLowercase";
+            this.includeLowercase.Size = new System.Drawing.Size(108, 24);
+            this.includeLowercase.TabIndex = 1;
+            this.includeLowercase.Text = "Małe litery";
+            this.includeLowercase.UseVisualStyleBackColor = true;
+
+            this.includeCaptial.AutoSize = true;
+            this.includeCaptial.Location = new System.Drawing.Point(117, 29);
+            this.includeCaptial.Margin = new System.Windows.Forms.Padding(2);
+            this.includeCaptial.Name = "includeCaptial";
+            this.includeCaptial.Size = new System.Drawing.Size(108, 24);
+            this.includeCaptial.TabIndex = 1;
+            this.includeCaptial.Text = "Duże litery";
+            this.includeCaptial.UseVisualStyleBackColor = true;
+
+            this.includeSpecCharctor.AutoSize = true;
+            this.includeSpecCharctor.Location = new System.Drawing.Point(0, 63);
+            this.includeSpecCharctor.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.includeSpecCharctor.Name = "includeSpecCharctor";
+            this.includeSpecCharctor.Size = new System.Drawing.Size(141, 24);
+            this.includeSpecCharctor.TabIndex = 0;
+            this.includeSpecCharctor.Text = "Znaki specjalne";
+            this.includeSpecCharctor.UseVisualStyleBackColor = true;
+            this.includeSpecCharctor.CheckedChanged += new System.EventHandler(this.includeSpecCharctor_CheckedChanged);
+
+            this.GenerateButton.Location = new System.Drawing.Point(139, 229);
+            this.GenerateButton.Margin = new System.Windows.Forms.Padding(2);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(159, 34);
+            this.GenerateButton.TabIndex = 2;
+            this.GenerateButton.Text = "Generuj hasło";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+
+            this.passwordBox.Location = new System.Drawing.Point(11, 279);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.ReadOnly = true;
+            this.passwordBox.Size = new System.Drawing.Size(412, 27);
+            this.passwordBox.TabIndex = 3;
+
+            this.CopyButton.Location = new System.Drawing.Point(26, 321);
+            this.CopyButton.Margin = new System.Windows.Forms.Padding(2);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(164, 34);
+            this.CopyButton.TabIndex = 4;
+            this.CopyButton.Text = "Skopiuj do schowka";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+
+            this.aboutButton.Location = new System.Drawing.Point(185, 401);
+            this.aboutButton.Margin = new System.Windows.Forms.Padding(2);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.Size = new System.Drawing.Size(109, 34);
+            this.aboutButton.TabIndex = 5;
+            this.aboutButton.Text = "O aplikacji...";
+            this.aboutButton.UseVisualStyleBackColor = true;
+            this.aboutButton.Click += new System.EventHandler(this.aboutButton_Click);
+
+            this.exitButton.Location = new System.Drawing.Point(305, 401);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(109, 34);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Wyjście";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+
+            this.savePasswordButton.Location = new System.Drawing.Point(240, 321);
+            this.savePasswordButton.Margin = new System.Windows.Forms.Padding(2);
+            this.savePasswordButton.Name = "savePasswordButton";
+            this.savePasswordButton.Size = new System.Drawing.Size(161, 34);
+            this.savePasswordButton.TabIndex = 6;
+            this.savePasswordButton.Text = "Zapisz do pliku";
+            this.savePasswordButton.UseVisualStyleBackColor = true;
+            this.savePasswordButton.Click += new System.EventHandler(this.savePasswordButton_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(434, 448);
+            this.Controls.Add(this.savePasswordButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generator haseł";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onFormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.onKeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        private System.Windows.Forms.CheckBox includeNums;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox passwordLengthBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox includeLowercase;
+        private System.Windows.Forms.CheckBox includeCaptial;
+        private System.Windows.Forms.CheckBox includeSpecCharctor;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button aboutButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button savePasswordButton;
+        private System.Windows.Forms.TextBox specCharacters;
+        private System.Windows.Forms.Button restoreDefault;
+        private System.Windows.Forms.RadioButton charTypeProbEqualButton;
+        private System.Windows.Forms.CheckBox generateGUID;
+    }
+}
+
---
+++ src/PasswordGenerator/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    enum CharType
+    {
+        CK_NUMBERS,
+        CK_CAPTIAL,
+        CK_LOWERCASE,
+        CK_SPECALCHAR
+    }
+
+    public partial class Form1 : Form
+    {
+        List<CharType> m_optionCheck = new List<CharType>();
+        Random m_randon = new Random();
+        string m_lastPassword = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            includeNums.Checked = Properties.Settings.Default.IncludeNum;
+            includeCaptial.Checked = Properties.Settings.Default.IncludeCapital;
+            includeLowercase.Checked = Properties.Settings.Default.IncludeLowercase;
+            includeSpecCharctor.Checked = Properties.Settings.Default.IncludeSpecCharator;
+            passwordLengthBox.Text = Properties.Settings.Default.PasswordLength;
+            specCharacters.Text = Properties.Settings.Default.SpecCharacters;
+            charTypeProbEqualButton.Checked = Properties.Settings.Default.CharTypeProbEqual;
+            generateGUID.Checked = Properties.Settings.Default.GenerateGUID;
+            SetControlEnableState();
+        }
+
+        private void GetOptionState()
+        {
+            m_optionCheck.Clear();
+            if (includeNums.Checked)
+                m_optionCheck.Add(CharType.CK_NUMBERS);
+            if (includeCaptial.Checked)
+                m_optionCheck.Add(CharType.CK_CAPTIAL);
+            if (includeLowercase.Checked)
+                m_optionCheck.Add(CharType.CK_LOWERCASE);
+            if (includeSpecCharctor.Checked)
+                m_optionCheck.Add(CharType.CK_SPECALCHAR);
+        }
+
+        private void SetControlEnableState()
+        {
+            specCharacters.Enabled = includeSpecCharctor.Checked && !generateGUID.Checked;
+            restoreDefault.Enabled = includeSpecCharctor.Checked && !generateGUID.Checked;
+
+            includeNums.Enabled = !generateGUID.Checked;
+            includeCaptial.Enabled = !generateGUID.Checked;
+            includeLowercase.Enabled = !generateGUID.Checked;
+            includeSpecCharctor.Enabled = !generateGUID.Checked;
+            label1.Enabled = !generateGUID.Checked;
+            passwordLengthBox.Enabled = !generateGUID.Checked;
+            charTypeProbEqualButton.Enabled = !generateGUID.Checked;
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (generateGUID.Checked)
+            {
+                string newGuid = Guid.NewGuid().ToString();
+                passwordBox.Text = newGuid;
+                return;
+            }
+
+            GetOptionState();
+            passwordBox.Text = "";
+            if (passwordLengthBox.Text.Length == 0)
+            {
+                MessageBox.Show("Wprowadź długość generowanego hasła!", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if(m_optionCheck.Count == 0)
+            {
+                MessageBox.Show("Wybierz typ znaków do uwzględnienia!", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int passwordLength = 0;
+            try
+            {
+                passwordLength = int.Parse(passwordLengthBox.Text);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Można wprowadzić tylko liczby do pola 'Długość hasła'!", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if(passwordLength == 0)
+            {
+                MessageBox.Show("Długość hasła nie może wynosić 0!", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (charTypeProbEqualButton.Checked)
+            {
+                for (int i = 0; i< passwordLength; i++)
+                {
+                    int index = m_randon.Next(m_optionCheck.Count);
+                    CharType charType = m_optionCheck[index];
+                    char currentChar = '\0';
+                    switch(charType)
+                    {
+                        case CharType.CK_NUMBERS:
+                            currentChar = Convert.ToChar(m_randon.Next(48, 58));
+                            break;
+                        case CharType.CK_CAPTIAL:
+                            currentChar = Convert.ToChar(m_randon.Next(65, 91));
+                            break;
+                        case CharType.CK_LOWERCASE:
+                            currentChar = Convert.ToChar(m_randon.Next(97, 123));
+                            break;
+                        case CharType.CK_SPECALCHAR:
+                            int randon = m_randon.Next(specCharacters.Text.Length);
+                            currentChar = specCharacters.Text[randon];
+                            break;
+                    }
+                    passwordBox.Text += currentChar;
+                }
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            if(passwordBox.TextLength > 0)
+            {
+                passwordBox.SelectAll();
+                passwordBox.Copy();
+                MessageBox.Show("Hasło zostało skopiowane do schowka.", "Wskazówka", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void onKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+                GenerateButton_Click(new object(), new EventArgs());
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            (new AboutForm()).ShowDialog();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void savePasswordButton_Click(object sender, EventArgs e)
+        {
+            if(passwordBox.TextLength > 0 && passwordBox.Text != m_lastPassword)
+            {
+                StreamWriter streamWriter = new StreamWriter(".\\password.log", true, Encoding.UTF8);
+                streamWriter.Write(DateTime.Now.ToString());
+                streamWriter.Write("：");
+                streamWriter.WriteLine(passwordBox.Text);
+                streamWriter.Close();
+                m_lastPassword = passwordBox.Text;
+                MessageBox.Show("Hasło zostało zapisane do pliku password.log.", "Wskazówka", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void onFormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.IncludeNum = includeNums.Checked;
+            Properties.Settings.Default.IncludeCapital = includeCaptial.Checked;
+            Properties.Settings.Default.IncludeLowercase = includeLowercase.Checked;
+            Properties.Settings.Default.IncludeSpecCharator = includeSpecCharctor.Checked;
+            Properties.Settings.Default.PasswordLength = passwordLengthBox.Text;
+            Properties.Settings.Default.SpecCharacters = specCharacters.Text;
+            Properties.Settings.Default.CharTypeProbEqual = charTypeProbEqualButton.Checked;
+            Properties.Settings.Default.GenerateGUID = generateGUID.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void restoreDefault_Click(object sender, EventArgs e)
+        {
+            specCharacters.Text = "~!@#$%^&*()_-+={}[]|\\<>/?";
+        }
+
+        private void includeSpecCharctor_CheckedChanged(object sender, EventArgs e)
+        {
+            SetControlEnableState();
+        }
+
+        private void generateGUID_CheckedChanged(object sender, EventArgs e)
+        {
+            SetControlEnableState();
+        }
+    }
+}
